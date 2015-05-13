cadb4d256f212476c24f7eeb7266936a6aaa1397
jmp9292 <josema92@gmail.com>
2015-05-13 17:05:55 +0200

    Añadidos mensajes de conexion exitosa o error al conectar con la Base de datos
    Signed-off-by: jmp9292 <josema92@gmail.com>

--- Bingo/src/Lanzador.java
+++ Bingo/src/Lanzador.java
@@ -1,6 +1,8 @@
 
 import Modelo.ConexionDB;
 import Modelo.modeloCorredores;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
 import Principal.Primera;
 
@@ -16,10 +18,20 @@ public class Lanzador {
 				//Generamos el objeto
 				Bingo=new ConexionDB ("91.121.110.152","Bingo","rootbingo","bingoplayers");
 				
-				if (Bingo.connectDB()==true) System.out.println("Conectados con éxito");
-				else System.out.println("Error en la conexión");	
+				if (Bingo.connectDB()==true) 
+				//System.out.println("Conectados con éxito");
+				JOptionPane.showMessageDialog (null, "Conectado con exito  a la BD "
+						+ "\n YA PUEDES JUGAR", "CONECTADO", JOptionPane.INFORMATION_MESSAGE);
+			//	JOptionPane.showMessageDialog (null, "Message", "Title", JOptionPane.WARNING_MESSAGE);
 				
-		
+				
+				
+				else //System.out.println("Error en la conexión");	
+					JOptionPane.showMessageDialog(null, "No podemos coenctar con la BD"
+							+ "\n y es necesaria para jugar", "ERROR AL CONECTAR", JOptionPane.ERROR_MESSAGE);
+							System.exit(0);
+				
+				//JOptionPane.showMessageDialog (null, "Message", "Title", JOptionPane.ERROR_MESSAGE);
 			
 		
 		
